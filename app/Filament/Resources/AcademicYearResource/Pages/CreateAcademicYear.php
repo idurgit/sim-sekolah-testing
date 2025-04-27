0555d1967e6102ea7711404d05e000b8096868c7
idurgit <idursupra@gmail.com>
2025-04-27 13:30:20 +0700

    academic year

---
+++ app/Filament/Resources/AcademicYearResource/Pages/CreateAcademicYear.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\AcademicYearResource\Pages;
+
+use App\Filament\Resources\AcademicYearResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAcademicYear extends CreateRecord
+{
+    protected static string $resource = AcademicYearResource::class;
+    protected static bool $canCreateAnother = false;
+
+    //customize redirect after create
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Models/AcademicYear.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class AcademicYear extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'year',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'integer',
+        ];
+    }
+
+    public function classrooms(): HasMany
+    {
+        return $this->hasMany(Classroom::class);
+    }
+}
